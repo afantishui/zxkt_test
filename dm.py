5d59281b3b212e6d546cb9435540ea49f92a8e3e
lijuhui <lijuhui@zy.com>
2019-06-21 18:51:55 +0800

    调试大漠

--- dm.py
+++ dm.py
@@ -4,7 +4,17 @@ print('12')
 dm = win32com.client.Dispatch('dm.dmsoft') # 调用大漠插件
 print(dm.ver()) # 输出版本号
 # 鼠键方法正常
-dm.MoveTo(10,20)
-print(dm.GetCursorPos())
+# dm.MoveTo(10,20)
+# print(dm.GetCursorPos())
+# 文字识别
+base_path = dm.GetBasePath()
+dm_ret = dm.SetPath(base_path)
+dm_ret = dm.SetDict(0,"ys.txt")
+s = dm.Ocr(835,529,925,553,"903a00-3a003a|003a90-b6ffff|000000-000000",0.8)
+
+if s is None:
+ 	print('没有识别到内容')
+else:
+ 	print('识别到:'+s)
 # 启动应用方法调试不成功
-dm.RunApp('notepad',1)
+# dm.RunApp('notepad',1)
--- operation.py
+++ operation.py
@@ -1,5 +1,11 @@
 # -*- coding:utf-8 -*- 
 from base import Base
+import win32com.client
+#调用大漠插件前需要把插件注册到系统
+dm = win32com.client.Dispatch('dm.dmsoft') # 调用大漠插件
+base_path = dm.GetBasePath()
+dm_ret = dm.SetPath(base_path)
+dm_ret = dm.SetDict(0,"ys.txt")
 app = Base()
 
 # 下载客户端
@@ -14,7 +20,7 @@ def download_client(url,path,filename):
 
 
 # 安装客户端
-def install_teacher(path,softname,classname):
+def install_teacher(path,softname,classname,path1):
 		# 启动安装文件
 		app.launch(path,softname)
 		app.wait(3)
@@ -22,13 +28,17 @@ def install_teacher(path,softname,classname):
 		hwnd = app.findwindow(classname)
 
 		if hwnd > 0 :
-			app.wait(2)
+			app.wait(0.5)
+			app.send_key('BackSpace')
+			app.wait(1)
+			app.text_en(b'D:\\Program Files (x86)\\NiushibangPCT',hwnd)
+			app.wait(1)
 			app.send_key('Enter')
-			app.wait(2)
-			app.send_key('Enter')
-			app.wait(10)
-			app.send_key('Enter')
-			app.wait(2)
+			# app.wait(2)
+			# app.send_key('Enter')
+			# app.wait(10)
+			# app.send_key('Enter')
+			# app.wait(2)
 			try:
 				hwnd = app.findwindow(classname)
 				print("关闭安装窗口后句柄")
@@ -36,6 +46,15 @@ def install_teacher(path,softname,classname):
 				print('安装完成')
 		else:
 			print('没有启动安装程序')
+
+# 启动应用
+def start_app():
+	softname = 'nsb-teacher.exe'
+	print('启动应用路径:'+"D:\\Program Files (x86)\\NiushibangPCT\\"+softname)
+	app.launch("D:\\Program Files (x86)\\NiushibangPCT\\", softname)
+	app.wait(3)
+
+
 
 # 卸载客户端
 def uninstall(path,softname):
@@ -60,25 +79,44 @@ def uninstall(path,softname):
 			print('没有启动安装程序')
 
 # 登录教师,账号密码续作参数化
-def login(classname,pos):
+def login(classname,pos,name,psd):
 	a = True
+
 	while a:
-	
 		a = False
 		hwnd = app.findwindow(classname)
+		s = dm.Ocr(827,527,955,556,"903a00-3a003a|003a90-b6ffff|000000-000000",0.9)
+		
 		# 输入账号
-		app.click(pos[0],'l')
+		app.click(pos[1],'l')
 		app.wait(0.5)
-		app.text_en(b'afanti',hwnd)
+		app.text_en(name,hwnd)
 		app.wait(0.5)
 		# 输入密码
 		app.send_key('Enter')
 		app.wait(0.5)
-		app.text_en(b'123456',hwnd)
+		app.text_en(psd,hwnd)
 		app.wait(0.5)
 		# 点确定
 		app.send_key('Enter')
 		print('ok')
+
+# 设备检测
+def s_device(classname,title):
+	hwnd_d = app.findwindow1(classname,title)
+	if hwnd_d > 0:
+		print('设备检测窗口正常打开')
+		# 设备检测窗口关闭按钮坐标
+		app.click(1340,290)
+	else:
+		print('没有检测到设备检测窗口')
+
+# 课表
+def s_course():
+	pass
+# 我的
+def s_myself():
+	pass
 
 # 调试
 def test():
