e171ac05fd29857c3ae5a6aa7d16b5915570b38e
afantishui <824144294@qq.com>
2019-09-30 18:36:25 +0800

    新增检测更新窗口位置

--- dm.py
+++ dm.py
@@ -1,8 +1,7 @@
 import win32com.client
-print('12')
 #调用大漠插件前需要把插件注册到系统
 dm = win32com.client.Dispatch('dm.dmsoft') # 调用大漠插件
-print(dm.ver()) # 输出版本号
+print('大漠系统版本'+dm.ver()) # 输出版本号
 # 鼠键方法正常
 # dm.MoveTo(10,20)
 # print(dm.GetCursorPos())
@@ -10,7 +9,9 @@ print(dm.ver()) # 输出版本号
 base_path = dm.GetBasePath()
 dm_ret = dm.SetPath(base_path)
 dm_ret = dm.SetDict(0,"ys.txt")
-s = dm.Ocr(835,529,925,553,"903a00-3a003a|003a90-b6ffff|000000-000000",0.8)
+# 账号 835,529,925,553,"903a00-3a003a|003a90-b6ffff|000000-000000",0.9
+# 检测更新窗口 923,321,999,341,"ffffff-ffca6e",0.9
+s = dm.Ocr(923,321,999,341,"ffffff-ffca6e",0.9) 
 
 if s is None:
  	print('没有识别到内容')
